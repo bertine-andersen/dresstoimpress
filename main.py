5165ffde63ab9957d922c817ba1eed3088b36ad3
Klara <klarauvaagdanielsen@gmail.com>
2026-01-13 12:35:53 +0100

    Forslag kode

--- main.py
+++ main.py
@@ -8,6 +8,26 @@ VINDU_BREDDE = 500
 VINDU_HOYDE  = 500
 vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
 
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_1:
+                current_sweater = 0
+            if event.key == pg.K_2:
+                current_sweater = 1
+            if event.key == pg.K_3:
+                current_sweater = 2
+
+            if event.key == pg.K_q:
+                current_pants = 0
+            if event.key == pg.K_w:
+                current_pants = 1
+            if event.key == pg.K_e:
+                current_pants = 2
 
 # Initialiserer/starter pygame
 pg.init()
