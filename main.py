fe011538b29fdfb402edd140ade5260a30f7b4d9
Klara <klarauvaagdanielsen@gmail.com>
2026-01-13 12:33:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import pygame as pg
 
+# Initialiserer/starter pygame
+pg.init()
+
+# Oppretter et vindu der vi skal "tegne" innholdet vårt
+VINDU_BREDDE = 500
+VINDU_HOYDE  = 500
+vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
+
+
+# Initialiserer/starter pygame
+pg.init()
