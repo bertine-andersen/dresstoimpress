e60ec68bdce71cf0666ec76448e1956fed7ecde1
Klara <klarauvaagdanielsen@gmail.com>
2026-01-14 09:47:43 +0100

    dagens antrekk

--- main.py
+++ main.py
@@ -63,12 +63,12 @@ bukser = [
 valgt_bukse = 0
 
 
-font = pg.font.SysFont("Arial", 10)
+font = pg.font.SysFont("Arial", 20)
 
 
 
 running = True
-
+ferdig_trykket = False  # NY VARIABEL!
 
 #knapp = pg.draw.rect(vindu, (255,192,203),(203,450,100,40) )
 knapp = pg.Rect(200,450,100,40)
@@ -81,12 +81,36 @@ while running:
         if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             running = False
         if event.type == pg.MOUSEBUTTONDOWN and knapp.collidepoint(event.pos):
-            print("Du klikket")
+            if not ferdig_trykket:  # Bare hvis ikke allerede trykket
+                print("Du klikket")
+                ferdig_trykket = True  # Sett til True!
+
             
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_1:
+                valgt_genser = 0
+            if event.key == pg.K_2:
+                valgt_genser = 1
+            if event.key == pg.K_3:
+                valgt_genser = 2
+            if event.key == pg.K_4:
+                valgt_genser = 3
+            if event.key == pg.K_5:
+                valgt_genser = 4
+
+            if event.key == pg.K_q:
+                valgt_bukse = 0
+            if event.key == pg.K_w:
+                valgt_bukse = 1
+            if event.key == pg.K_e:
+                valgt_bukse = 2
+            if event.key == pg.K_r:
+                valgt_bukse = 3
+            if event.key == pg.K_t:
+                valgt_bukse = 4
 
 
-
-    
+                
         # Fyll bakgrunn (valgfritt, men anbefalt)
         vindu.fill((255, 255, 255))
 
@@ -112,34 +136,20 @@ while running:
         vindu.blit(bilde, (knapp.x+15, knapp.y+5))
 
         
-
-        
+        if ferdig_trykket:
+        # Vis "Dagens antrekk!" øverst
+            finish = font.render("Dagens antrekk!", True, (255, 0, 0))
+            vindu.blit(finish, (150, 20))
+    
+        else:
+            # Vis "Ferdig"-knappen
+            pg.draw.rect(vindu, (255, 192, 203), knapp)
+            bilde = font.render("Ferdig", True, (255, 0, 0))
+            vindu.blit(bilde, (knapp.x + 15, knapp.y + 5))
+            
         pg.display.flip()
 
 
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_1:
-                valgt_genser = 0
-            if event.key == pg.K_2:
-                valgt_genser = 1
-            if event.key == pg.K_3:
-                valgt_genser = 2
-            if event.key == pg.K_4:
-                valgt_genser = 3
-            if event.key == pg.K_5:
-                valgt_genser = 4
-
-            if event.key == pg.K_q:
-                valgt_bukse = 0
-            if event.key == pg.K_w:
-                valgt_bukse = 1
-            if event.key == pg.K_e:
-                valgt_bukse = 2
-            if event.key == pg.K_r:
-                valgt_bukse = 3
-            if event.key == pg.K_t:
-                valgt_bukse = 4
-    
 
 
 
